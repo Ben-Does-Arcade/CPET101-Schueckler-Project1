ede5d39fbda1afb839183e26e5d50dc25a687b2e
Ben <bendoesarcade@gmail.com>
2023-09-22 18:32:44 -0400

    Updated project documentation and code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
-//
-// Created by ben_s on 9/22/2023.
-//
+// **********************************************************************
+// Title: main.cpp
+// Course: Computational Problem Solving I
+// Author: Ben Stafford
+// Date: 22Sep23
+// Description: This program calculates the numerical and letter grade
+//              for a single student.
+// **********************************************************************
 
 #include <iostream>
 #include <iomanip>
@@ -9,7 +14,7 @@
 using namespace std;
 
 int main() {
-    // My code goes here
+    // My code will go here soon
 
     return 0;
 }
