0f7e26e7cb1affb6f19f0f829c0db6cab7ad1543
Ben <bendoesarcade@gmail.com>
2023-09-22 11:32:01 -0400

    Updated documentation and code

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
 //
 // Created by ben_s on 9/22/2023.
 //
+
+#include <iostream>
+#include <iomanip>
+#include <map>
+
+using namespace std;
+
+int main() {
+    // My code goes here
+
+    return 0;
+}
